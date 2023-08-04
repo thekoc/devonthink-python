9338ff4925699843c61dbf7ccee11154edc1bbb4
thekoc <robot.koc@outlook.com>
2023-08-05 02:36:52 +0800

    Updated test cases

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,11 @@
 import unittest
 import typing
 import logging
+import re
 from pydt3 import DEVONthink3
+from pydt3.osascript import OSAScript, OSAObjProxy, OSAObjArray
+from pydt3.models.record import Record
+from pydt3.models.smartgroup import SmartGroup
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -12,36 +16,108 @@ class TestDatabase(unittest.TestCase):
         super().__init__(methodName)
         dbs = DEVONthink3().databases
         assert len(dbs) > 0, "No databases found"
-        self.dbs = dbs
+        self.dbs = list(dbs)
     
-    def test_properties(self):
+    def test_contents(self):
         for db in self.dbs:
-            self._test_db(db)
+            contents = db.contents
+            self.assertTrue(isinstance(contents, typing.Sequence))
+            self.assertTrue(all(isinstance(record, Record) for record in contents))
     
-    def _test_db(self, db):
-        obj = db
-        type_ = type(obj)
-        for name in dir(type_):
-            try:
-                if name.startswith('_'):
-                    continue
-                if isinstance(getattr(type_, name), property):
-                    logger.info(f"Testing {obj}.{name}")
-                    returned_value = getattr(obj, name)
-                    returned_type = typing.get_type_hints(getattr(type_, name).fget)['return']
-                    generic_origin = typing.get_origin(returned_type)
-                    if generic_origin is not None and generic_origin is list:
-                        generic_args = typing.get_args(returned_type)
-                        assert len(generic_args) == 1, f"Expected 1 generic argument, got {len(generic_args)}"
-                        self.assertTrue(all(isinstance(value, generic_args[0]) for value in returned_value), f"Expected all values of {type_}.{name} to be of type {generic_args[0]}, got {returned_value}")
-                    else:
-                        self.assertIsInstance(returned_value, returned_type)
-            except NotImplementedError:
-                continue
+    def test_parents(self):
+        for db in self.dbs:
+            parents = db.parents
+            self.assertTrue(isinstance(parents, typing.Sequence))
+            self.assertTrue(all(isinstance(record, Record) for record in parents))
 
-            
+    def test_records(self):
+        for db in self.dbs:
+            records = db.records
+            self.assertTrue(isinstance(records, typing.Sequence))
+            self.assertTrue(all(isinstance(record, Record) for record in records))
 
+    def test_smart_groups(self):
+        for db in self.dbs:
+            smart_groups = db.smart_groups
+            self.assertTrue(isinstance(smart_groups, typing.Sequence))
+            self.assertTrue(all(isinstance(smart_group, SmartGroup) for smart_group in smart_groups))
 
+    def test_annotations_group(self):
+        for db in self.dbs:
+            annotations_group = db.annotations_group
+            self.assertTrue(isinstance(annotations_group, Record))
+
+    def test_comment(self):
+        for db in self.dbs:
+            comment = db.comment
+            self.assertTrue(isinstance(comment, str))
+            # Test the setter method
+            old_comment = comment
+            db.comment = "test"
+            self.assertEqual(db.comment, "test")
+            # Revert the change
+            db.comment = old_comment
+
+    def test_current_group(self):
+        for db in self.dbs:
+            current_group = db.current_group
+            self.assertTrue(isinstance(current_group, Record))
+    def test_encrypted(self):
+        for db in self.dbs:
+            encrypted = db.encrypted
+            self.assertTrue(isinstance(encrypted, bool))
+
+    def test_id(self):
+        for db in self.dbs:
+            id = db.id
+            self.assertTrue(isinstance(id, int))
+
+    def test_incoming_group(self):
+        for db in self.dbs:
+            incoming_group = db.incoming_group
+            self.assertTrue(isinstance(incoming_group, Record))
+
+    def test_name(self):
+        for db in self.dbs:
+            name = db.name
+            self.assertTrue(isinstance(name, str))
+            # Test the setter method
+            old_name = name
+            db.name = "test"
+            self.assertEqual(db.name, "test")
+            # Revert the change
+            db.name = old_name
+
+    def test_path(self):
+        for db in self.dbs:
+            path = db.path
+            self.assertTrue(isinstance(path, str))
+            self.assertTrue(re.match(r'^/.*\.dtBase2$', path))
+
+    def test_read_only(self):
+        for db in self.dbs:
+            read_only = db.read_only
+            self.assertTrue(isinstance(read_only, bool))
+
+    def test_root(self):
+        for db in self.dbs:
+            root = db.root
+            self.assertTrue(isinstance(root, Record))
+
+    def test_tags_group(self):
+        for db in self.dbs:
+            tags_group = db.tags_group
+            self.assertTrue(isinstance(tags_group, Record))
+
+    def test_trash_group(self):
+        for db in self.dbs:
+            trash_group = db.trash_group
+            self.assertTrue(isinstance(trash_group, Record))
+
+    def test_uuid(self):
+        for db in self.dbs:
+            uuid = db.uuid
+            self.assertTrue(isinstance(uuid, str))
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/test_devonthink_application.py
@@ -0,0 +1,34 @@
+import unittest
+import logging
+import typing
+
+from pydt3 import DEVONthink3
+from pydt3.application import Application
+from pydt3.models.database import Database
+from pydt3.osascript import OSAObjArray
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+class TestDEVONhinkApplication(unittest.TestCase):
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.app = DEVONthink3()
+
+    def test_id(self):
+        print(self.app.id)
+        self.assertTrue(self.app.id == 'com.devon-technologies.think3')
+        
+    def test_name(self):
+        self.assertTrue(self.app.name == 'DEVONthink 3')
+    
+    def test_databases(self):
+        dbs  = self.app.databases
+        self.assertTrue(isinstance(dbs, OSAObjArray))
+        self.assertTrue(len(dbs) > 0)
+        self.assertTrue(all(isinstance(db, Database) for db in dbs))
+
+
+if __name__ == '__main__':
+    unittest.main()
